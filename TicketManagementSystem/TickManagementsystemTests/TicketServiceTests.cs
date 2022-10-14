0d7d133ca890997a927ce9d44867fd097f504a7d
j-a-k <jms.keogh@gmail.com>
2022-10-14 14:58:08 +0100

    Set up a test project

---
+++ TicketManagementSystem/TickManagementsystemTests/TicketServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+namespace TickManagementsystemTests
+{
+
+
+    [TestFixture]
+    public class TicketServiceTests
+    {
+        [Test]
+        public void CreateTicket()
+        {
+            Assert.Fail();
+        }
+    }
+}
